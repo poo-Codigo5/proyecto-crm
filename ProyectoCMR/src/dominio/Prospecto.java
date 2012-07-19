6b4f2e57b41d86c8bad50e44c3929eda0fecdeba
Jorge Cabezudo Perez <u201215490@upc.edu.pe>
2012-07-18 21:46:11 -0500

    Creacion de Clase Prospecto
    Creacion de clase Prospecto y controlador para agregar datos y buscar en
    un arreglo interno al controlador simulando los datos

--- ProyectoCMR/src/dominio/Prospecto.java
+++ ProyectoCMR/src/dominio/Prospecto.java
@@ -8,13 +8,58 @@ public class Prospecto {
 	private String DNI;
 	private String telefono;
 	private String fechaContacto;
-	public boolean agregar(String p_nombres, String p_apellidoPaterno, String p_apellidoMaterno,
-			String p_DNI, String p_telefono, String p_fechaContacto) {
-		// TODO Auto-generated method stub
-		return true;
+	
+	public Prospecto() {
+		this.nombres = "";
+		this.apellidoPaterno = "";
+		this.apellidoMaterno = "";
+		this.eMail = "";
+		this.DNI = "";
+		this.telefono = "";
+		this.fechaContacto = "";
 	}
-	public boolean buscar(String p_filtro, String p_dato) {
-		// TODO Auto-generated method stub
-		return false;
+	public void setNombres(String p_nombres) {
+		this.nombres = p_nombres;
 	}
+	public void setApellidoPaterno(String p_apellidoPaterno) {
+		this.apellidoPaterno = p_apellidoPaterno;
+	}
+	public void setApellidoMaterno(String p_apellidoMaterno) {
+		this.apellidoMaterno = p_apellidoMaterno;
+	}
+	public void setEMail(String p_eMail) {
+		this.eMail = p_eMail;
+	}
+	public void setDNI(String p_DNI) {
+		this.DNI = p_DNI;
+	}
+	public void setTelefono(String p_telefono) {
+		this.telefono = p_telefono;
+	}
+	public void setFechaContacto(String p_fechaContacto) {
+		this.fechaContacto = p_fechaContacto;
+	}
+	
+	public String getNombres() {
+		return this.nombres;
+	}
+	public String getApellidoPaterno() {
+		return this.apellidoPaterno;
+	}
+	public String getApellidoMaterno() {
+		return this.apellidoMaterno;
+	}
+	public String getEMail() {
+		return this.eMail;
+	}
+	public String getDNI() {
+		return this.DNI;
+	}
+	public String getTelefono() {
+		return this.telefono;
+	}
+	public String getFechaContacto() {
+		return this.fechaContacto;
+	}
+	
 }
--- ProyectoCMR/test/testDominio/ProspectoTest.java
+++ ProyectoCMR/test/testDominio/ProspectoTest.java
@@ -5,15 +5,15 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import dominio.Prospecto;
+import controlador.ProspectoControlador;
 
 public class ProspectoTest {
-	Prospecto prospecto = new Prospecto();
+	ProspectoControlador prospectoControlador = new ProspectoControlador();
 	
 	@Test
 	public void testAgregarProspecto() {
-		assertTrue(prospecto.agregar("Juan","Perez","Rojas","juanperez@hotmail.com","16201123","720-1222"));
-		assertTrue(prospecto.buscar("nombre","Juan"));
-		assertFalse(prospecto.buscar("DNI","12345678"));
+		assertTrue(prospectoControlador.agregar("Juan","Perez","Rojas","juanperez@hotmail.com","16201123","720-1222","01/01/2012"));
+		assertTrue(prospectoControlador.buscar("nombres","Juan"));
+		assertFalse(prospectoControlador.buscar("DNI","12345678"));
 	}
 }
