d3dbf619657c34637a57f5705bfcb2b8e1d15b9b
Jorge Cabezudo Perez <u201215490@upc.edu.pe>
2012-07-21 20:43:19 -0500

    Actualizacion CRM

---
+++ ProyectoCMR/src/controlador/Main.java
@@ -0,0 +1,14 @@
+package controlador;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu menu = new Menu();
+		menu.show();
+	}
+
+}
